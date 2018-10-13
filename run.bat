3dd3281a4d0d1238a9a21407a75a52de20914207
LochStudios <lachlan@lochstudios.com>
2018-10-13 13:59:53 +1100

    Update run.bat
    Fixed a bit of the code, removed the FTP code as I found a new way to get the files that we need.

--- run.bat
+++ run.bat
@@ -2,30 +2,32 @@
 :handles
 Color 6
 set server=##SERVER NAME WITH HELD FROM GITHUB##
+set server-map=T:\
 set installed=%appdata%\.minecraft\
 set V=1.0
 set name=Minecraft Launcher %V%
 title %name%
 cls
 
-:server?
+:map-server
+NET USE T: %server%\gfalaunchcraft\
+goto server
+
+:server
 echo.
 echo Is there a server?
 echo.
-IF EXIST "%server%\Minecraft\" (goto V-Check1) ELSE (goto V-Check2)
+IF EXIST "%server-map%\is-server.txt" (goto V-Check1) ELSE (goto V-Check2)
 
 :V-Check1
 cls
 echo Checking version
-IF EXIST "%server%\Minecraft\%V%.txt" (echo Match) ELSE (goto GetUpdate1)
+IF EXIST "%server-map%\%V%.txt" (echo Match) ELSE (goto GetUpdate1)
 
 :V-Check2
 ::VCheck.ftp
 cls
 echo Checking version
-REM .In
-REM FTP -S:VCheck.ftp [ftp.gamingforaustralia.com.au]
-REM .Out
 IF EXIST "\%V%.txt" (echo Match) ELSE (goto GetUpdate2)
 goto run
 
@@ -37,7 +39,7 @@ echo.
 FTP -S:Getfiles.ftp [## NEW SERVER WITH HELD FROM GITHUB##]
 IF EXIST "\minecraft.zip" (goto ZipDown) ELSE (goto UnZip)
 REM .In
-REM Should be download the new version.
+REM Should be downloading the new version.
 REM .Out
 
 :continue
